2a1f37731f2036875d66042c57e6d593d9f5ab9b
Anthony Nicholson <anthony.m.nicholson@gmail.com>
2021-05-31 17:00:33 +0900

    refactored faq questions into a list

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  let buttons = document.querySelectorAll("#faq-questions button")
+  buttons.forEach(button => {
+    let content = button.nextElementSibling
+    button.addEventListener("click", (event) => {
+      if(button.classList.contains("active")){
+        button.classList.remove("active")
+        button.setAttribute("aria-expanded", false)
+        content.style.maxHeight = null
+        content.setAttribute("aria-hidden", true)
+       }
+      else{
+        button.classList.add("active")
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.setAttribute("aria-hidden", false)
+        button.setAttribute("aria-expanded", true)
+      }
+    })
+  })
+  console.log("hey");
+});
